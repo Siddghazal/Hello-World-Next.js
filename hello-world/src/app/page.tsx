4e217775e38810dc66a9c3d6ae588c1313de2151
sidd-ghazal <ghazal_siddiqui@hotmail.com>
2024-11-14 13:02:51 +0500

    first commit

---
+++ hello-world/src/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function home(){
+  return(
+    <div  className="flex items-center justify-center h-screen bg-gray-200">
+      <h1 className=" text-5xl text-blue-500  "><b> Hello World !</b></h1>
+    </div>
+  )
+}
